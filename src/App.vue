69f9a15abeda56bc436665a562d1f06fc97f6edd
pzjyyzs <463669395@qq.com>
2024-07-06 17:23:42 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import Test from './components/Test.vue'
+</script>
+
+<template>
+  <Test></Test>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/Test.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        first template
+    </div>
+</template>
